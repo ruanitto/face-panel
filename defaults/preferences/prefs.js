bb1a68b47f19e5a58ef08df6a129dd30eb9432cd
Rafael Gomes <rafael.gomes@madsolutions.com.br>
2017-03-29 13:38:04 -0300

    Alter version and Multiprocessor

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,7 +1,7 @@
 pref("extensions.facebook_panel.panelWidth", 50);
 pref("extensions.facebook_panel.panelHeight", 90);
 pref("extensions.facebook_panel.isFirstRun", true);
-pref("extensions.facebook_panel.delayFirstRun", 10);
+pref("extensions.facebook_panel.delayFirstRun", 15);
 pref("extensions.facebook_panel.modfiers", "shift alt");
 pref("extensions.facebook_panel.key", "f");
 pref("extensions.facebook_panel.noautohide", false);

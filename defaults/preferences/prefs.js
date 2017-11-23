e4de1e2a8b5f135374a8cb6e366e64ecee88ecfd
Rafael Gomes <rafael.gomes@madsolutions.com.br>
2017-11-23 15:01:33 -0200

    Atualização de codigo

--- defaults/preferences/prefs.js
+++
@@ -1,9 +0,0 @@
-pref("extensions.facebook_panel.panelWidth", 50);
-pref("extensions.facebook_panel.panelHeight", 90);
-pref("extensions.facebook_panel.isFirstRun", true);
-pref("extensions.facebook_panel.delayFirstRun", 15);
-pref("extensions.facebook_panel.modfiers", "shift alt");
-pref("extensions.facebook_panel.key", "f");
-pref("extensions.facebook_panel.noautohide", false);
-pref("extensions.facebook_panel.toolbarAutoHide", false);
-pref("extensions.facebook_panel.faviconRefreshTime", 1);
